78ea94ee0983f3abf0e9b8d4f9a6a4b7dc511c9c
Tranzy <itholen@gmail.com>
2011-03-12 13:58:23 -0800

    Search formulier van default template mergen met die van we1rdo.

--- templates_we1rdo/filters.inc.php
+++ templates_we1rdo/filters.inc.php
@@ -1,41 +1,38 @@
-				<div class="filter">
-					<h4>Zoeken</h4>
+<div class="filtercontainer">
+            
+                <div class="filter shadow">
+                    <h4>Zoeken</h4>
 
-					<form id="filterform" action="">
+                    <form id="filterform" action="">
 <?php
-	$search = array_merge(array('type' => 'Titel', 'text' => '', 'tree' => '', 'unfiltered' => ''), $search);
+    $search = array_merge(array('type' => 'Titel', 'text' => '', 'tree' => '', 'unfiltered' => ''), $search);
 ?>
-						<input type="hidden" id="search-tree" name="search[tree]" value="<?php echo $search['tree']; ?>"></input>
-						<table class="filters">
-							<tbody>
-								<tr> 
-									<td> <input type="radio" name="search[type]" value="Titel"  <?php echo $search['type'] == "Titel" ? 'checked="checked"' : "" ?>>Titel</input> </td>
-									<td> <input type="radio" name="search[type]" value="Poster" <?php echo $search['type'] == "Poster" ? 'checked="checked"' : "" ?>>Afzender</input> </td>
-									<td> <input type="radio" name="search[type]" value="Tag"	<?php echo $search['type'] == "Tag" ? 'checked="checked"' : "" ?>>Tag</input> </td>
-								</tr>
-								
-								<tr>
-									<td colspan="3"><input class='searchbox' type="text" name="search[text]" value="<?php echo htmlspecialchars($search['text']); ?>"></input></td>
-								</tr>
+                    <input type="hidden" id="search-tree" name="search[tree]" value="<?php echo $search['tree']; ?>"></input>
+                        <table class="filters">
+                        <tr> 
+                            <td> <input type="radio" name="search[type]" value="Titel"  <?php echo $search['type'] == "Titel" ? 'checked="checked"' : "" ?>>Titel</input> </td>
+                            <td> <input type="radio" name="search[type]" value="Poster" <?php echo $search['type'] == "Poster" ? 'checked="checked"' : "" ?>>Afzender</input> </td>
+                            <td> <input type="radio" name="search[type]" value="Tag"    <?php echo $search['type'] == "Tag" ? 'checked="checked"' : "" ?>>Tag</input> </td>
+                        </tr>
+                        
+                        <tr>
+                            <td colspan="3"><input class='searchbox' type="text" name="search[text]" value="<?php echo htmlspecialchars($search['text']); ?>"></input></td>
+                        </tr>
 
-								<tr> 
-									<td colspan="3"> <input type="checkbox" name="search[unfiltered]" value="true"  <?php echo $search['unfiltered'] == "true" ? 'checked="checked"' : "" ?>>Vergeet filters voor zoekopdracht</input> </td>
-								</tr>
-							</tbody>
-						</table>
-
-						<div class='advancedsearch'>
-							<tr> 
-								<td colspan='3'> <input type="checkbox" name="search[unfiltered]" id='search-unfiltered' value="true"  <?php echo $search['unfiltered'] == "true" ? 'checked="checked"' : "" ?>></input><label for='search-unfiltered'>Vergeet filters voor zoekopdracht</label></td>
-							</tr>
-							<div id="tree"> 
-								<ul>
-								</ul>
-							</div>
-						</div>
-						
-						<input type='submit' class="filtersubmit" value='Zoek en filter'></input>
-					</form>
+                        <tr> 
+                            <td colspan='3'> <input type="checkbox" name="search[unfiltered]" value="true"  <?php echo $search['unfiltered'] == "true" ? 'checked="checked"' : "" ?>>Vergeet filters voor zoekopdracht</input> </td>
+                        </tr>
+                        </table>
+                    
+                        <div id="tree"> 
+                            <ul>
+                            </ul>
+                        </div>
+                        
+                        <br>
+                        <input type='submit' class="filtersubmit" value='Zoek en filter'></input>
+                    </form>
+                
 
 <h4>Filters</h4>
                     
@@ -49,26 +46,26 @@
             echo "\t\t\t\t\t\t\t<ul class='filterlist subfilterlist'>\r\n";
             foreach($filter[4] as $subFilter) {
 ?>
-            			<li> <a class="filter <?php echo $subFilter[3];?>" href="?search[tree]=<?php echo $subFilter[2];?>"><img src='<?php echo $subFilter[1]; ?>'><?php echo $subFilter[0]; ?></a>
+                        <li> <a class="filter <?php echo $subFilter[3];?>" href="?search[tree]=<?php echo $subFilter[2];?>"><img src='<?php echo $subFilter[1]; ?>'><?php echo $subFilter[0]; ?></a>
 <?php
-			} # foreach 
+            } # foreach 
             echo "\t\t\t\t\t\t\t</ul>\r\n";
         } # is_array
     } # foreach
 ?>
                     </ul>
 
-					<h4>Maintenance</h4>
-					<ul class="filterlist maintenancebox">
-						<li class="info"> Laatste update: <?php echo $tplHelper->formatDate($lastupdate, 'lastupdate'); ?> </li>
+                    <h4>Maintenance</h4>
+                    <ul class="filterlist maintenancebox">
+                        <li class="info"> Laatste update: <?php echo $tplHelper->formatDate($lastupdate, 'lastupdate'); ?> </li>
 <?php
-	if ($settings['show_updatebutton']) {
+    if ($settings['show_updatebutton']) {
 ?>
-						<li> <a href="retrieve.php?output=xml" id="updatespotsbtn" class="updatespotsbtn">Update Spots</a></li>
+                        <li> <a href="retrieve.php?output=xml" id="updatespotsbtn" class="updatespotsbtn">Update Spots</a></li>
 <?php
-	}
+    }
 ?>
-						<li> <a href="?page=erasedls" id="removedllistbtn" class="erasedlsbtn">Remove history of downloads</a></li>
-					</ul>
+                        <li> <a href="?page=erasedls" id="removedllistbtn" class="erasedlsbtn">Remove history of downloads</a></li>
+                    </ul>
 
-				</div>
+                </div>
